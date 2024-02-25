ca9b43c92f06f41064e236da3fc33542e4b05d52
Mihail Romanov <romanov.m@skbkontur.ru>
2024-02-25 21:33:44 +0400

    Fix build on Windows 7

--- clean.bat
+++ clean.bat
@@ -1,6 +1,7 @@
-@for /f "usebackq tokens=*" %%i in (`src\Tools\VSWhere\vswhere.exe -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
+@for /f "usebackq tokens=*" %%i in (`src\Tools\VSWhere\vswhere.exe -latest -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
   set msbuild=%%i
 )
+
 "%msbuild%" /m SharpDevelop.sln /t:clean "/p:Platform=Any CPU" /p:Configuration=Debug
 @IF %ERRORLEVEL% NEQ 0 PAUSE
 "%msbuild%" /m SharpDevelop.sln /t:clean "/p:Platform=Any CPU" /p:Configuration=Release
