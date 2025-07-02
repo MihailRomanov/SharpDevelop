ca720e8ac5dfc274232c48faa56fc6e18f169036
MihailRomanov <mihail_romanov@live.ru>
2025-07-02 21:35:15 +0400

    Disable VBBinding and add RoslynBinding

---
+++ src/AddIns/BackendBindings/RoslynBinding/Project/VBProject.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: romanov.m
+ * Date: 02.07.2025
+ * Time: 20:37
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using ICSharpCode.SharpDevelop.Project;
+
+namespace ICSharpCode.RoslynBinding
+{
+	/// <summary>
+	/// Description of VBProject.
+	/// </summary>
+	public class VBProject : CompilableProject
+	{
+		public VBProject(ProjectLoadInformation prjLoadInfo) : base(prjLoadInfo)
+		{}
+
+		public VBProject(ProjectCreateInformation prjCreateInfo) : base(prjCreateInfo)
+		{}
+		
+		#region implemented abstract members of CompilableProject
+
+		public override string Language {
+			get {
+				return VBProjectBinding.LanguageName;
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ src/AddIns/BackendBindings/RoslynBinding/Project/VBProjectBinding.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: romanov.m
+ * Date: 02.07.2025
+ * Time: 20:35
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using ICSharpCode.SharpDevelop.Project;
+
+namespace ICSharpCode.RoslynBinding
+{
+	/// <summary>
+	/// Description of VBProjectBinding.
+	/// </summary>
+	public class VBProjectBinding : IProjectBinding
+	{
+		public const string LanguageName = "VB";
+		
+		#region IProjectBinding implementation
+
+		public IProject LoadProject(ProjectLoadInformation info)
+		{
+			return new VBProject(info);
+		}
+
+		public IProject CreateProject(ProjectCreateInformation info)
+		{
+			return new VBProject(info);
+		}
+
+		public bool HandlingMissingProject {
+			get {
+				return false;
+			}
+		}
+
+		#endregion
+	}
+}
